916a075b2bbd696eaf257ef9852d6eb3479b18d4
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-08-19 00:16:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    return customerName = "BOB"
+}
+
+function setBestCustomer(){
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'sarah'
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'lara';
+}
